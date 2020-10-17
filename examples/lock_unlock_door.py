7ba86b20db311fcd3cbdf1349a5ce31e09d63bfc
Victor <utdseniordesignteam1060@gmail.com>
2020-10-17 18:40:11 -0500

    Fix servo motor bug and unlock angle

--- examples/lock_unlock_door.py
+++ examples/lock_unlock_door.py
@@ -2,11 +2,11 @@
 
 Segmented switch that we are using (one option will be highlighted at a time)
 
-UNLOCK sends a value of 1
-LOCK sends a value of 2
+LOCK sends a value of 1
+UNLOCK sends a value of 2
 
 +-----------------+
-| UNLOCK |  LOCK  |
+|  LOCK  | UNLOCK |
 +-----------------+
 
 """
@@ -15,69 +15,73 @@ import blynklib
 import RPi.GPIO as GPIO
 from time import sleep
 
-# insert your Auth Token here
 BLYNK_AUTH = "OXC2SHosxUj-wNyaeRMpEyd-JhHX7Vv9"
 
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
-UNLOCK = 1
-LOCK = 2
+LOCK = 1
+UNLOCK = 2
 
-UNLOCK_ANGLE = 180
 LOCK_ANGLE = 0
+UNLOCK_ANGLE = 90
 
 SERVO_PIN = 14
 PWM_FREQ = 50
 
-# Initial state is locked
-lock_state = LOCK
-
-GPIO.setmode(GPIO.BOARD)
-
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO_PIN, GPIO.OUT)
+
 servo = GPIO.PWM(SERVO_PIN, PWM_FREQ)
 
 
-# register handler for Virtual Pin V0 writing by Blynk App
 @blynk.handle_event("write V0")
-def write_virtual_pin_handler(pin, values):
-    # values is a list that contains a single string (the write value)
+def write_lock_virtual_pin_handler(pin, values):
     value = int(values[0])
 
-    if value == UNLOCK:
-        unlock_door()
-    elif value == LOCK:
+    if value == LOCK:
         lock_door()
+    elif value == UNLOCK:
+        unlock_door()
     else:
         print("Invalid value")
 
 
-def unlock_door():
-    move_servo_to_angle(180)
-
-    global lock_state
-    lock_state = UNLOCK
-
-    blynk.notify("Door has been unlocked")
-
-
 def lock_door():
-    move_servo_to_angle(0)
+    move_servo_to_angle(LOCK_ANGLE)
 
     global lock_state
     lock_state = LOCK
 
-    blynk.notify("Door has been locked")
+    print("Door has been locked")
+
+
+def unlock_door():
+    move_servo_to_angle(UNLOCK_ANGLE)
+
+    global lock_state
+    lock_state = UNLOCK
+
+    print("Door has been unlocked")
 
 
 def move_servo_to_angle(angle):
-    print(f"Changed angle to {angle}")
-    duty_cycle = angle / 18 + 2
-    servo.ChangeDutyCycle(duty_cycle)
-    sleep(1)
+    servo.ChangeDutyCycle(get_duty_cycle(angle))
+    sleep(0.5)
     servo.ChangeDutyCycle(0)
 
 
+def get_duty_cycle(angle):
+    return angle / 18 + 2
+
+
 if __name__ == "__main__":
-    while True:
-        blynk.run()
+    # Initial state is locked
+    servo.start(get_duty_cycle(LOCK_ANGLE))
+    lock_state = LOCK
+
+    try:
+        while True:
+            blynk.run()
+    finally:
+        servo.stop()
+        GPIO.cleanup()
