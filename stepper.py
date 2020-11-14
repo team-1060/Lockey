5055f9d8195685eb98af8a8ea08171f62c620f94
team-1060 <72582086+team-1060@users.noreply.github.com>
2020-11-13 21:03:36 -0600

    this works

--- stepper.py
+++ stepper.py
@@ -1,53 +1,17 @@
-import time
-import board
-import digitalio
+'''
+    Stepper Motor interfacing with Raspberry Pi
+    http:///www.electronicwings.com
+'''
+import RPi.GPIO as GPIO
+from time import sleep
+import sys
 import blynklib
- 
-enable_pin = digitalio.DigitalInOut(board.D18)
-coil_A_1_pin = digitalio.DigitalInOut(board.D4)
-coil_A_2_pin = digitalio.DigitalInOut(board.D17)
-coil_B_1_pin = digitalio.DigitalInOut(board.D23)
-coil_B_2_pin = digitalio.DigitalInOut(board.D24)
- 
-enable_pin.direction = digitalio.Direction.OUTPUT
-coil_A_1_pin.direction = digitalio.Direction.OUTPUT
-coil_A_2_pin.direction = digitalio.Direction.OUTPUT
-coil_B_1_pin.direction = digitalio.Direction.OUTPUT
-coil_B_2_pin.direction = digitalio.Direction.OUTPUT
 
-enable_pin.value = True
-
-def forward(delay, steps):
-    i = 0
-    while i in range(0, steps):
-        setStep(1, 0, 1, 0)
-        time.sleep(delay)
-        setStep(0, 1, 1, 0)
-        time.sleep(delay)
-        setStep(0, 1, 0, 1)
-        time.sleep(delay)
-        setStep(1, 0, 0, 1)
-        time.sleep(delay)
-        i += 1
-def backwards(delay, steps):
-    i = 0
-    while i in range(0, steps):
-        setStep(1, 0, 0, 1)
-        time.sleep(delay)
-        setStep(0, 1, 0, 1)
-        time.sleep(delay)
-        setStep(0, 1, 1, 0)
-        time.sleep(delay)
-        setStep(1, 0, 1, 0)
-        time.sleep(delay)
-        i += 1
- 
-def setStep(w1, w2, w3, w4):
-    coil_A_1_pin.value = w1
-    coil_A_2_pin.value = w2
-    coil_B_1_pin.value = w3
-    coil_B_2_pin.value = w4
- 
+#assign GPIO pins for motor
+motor_channel = (29,31,33,35)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(motor_channel, GPIO.OUT)
 auth_token = '4JR18qzs1AWkedrhF65mjwuASpBMpeGs'
 
 # Initialize Blynk
@@ -57,12 +21,53 @@ blynk = blynklib.Blynk(auth_token)
 @blynk.handle_event('write V1')
 def write_handler_pin_handler(pin, value):
     Doorstep = (format(value[0]))
-    if Doorstep =="0":
-        forward(int(2) / 1000.0, int(1500))
+    if Doorstep =="1":
+        print("open call")
+        stepper('c')
         print("Door open")
-    elif Doorstep =="1":
-        backwards(int(2) / 1000.0, int(1500))
+    elif Doorstep =="2":
+        print("open call")
+        stepper('a')
         print("Door closed")
+
+def stepper(direction):
+    x=0
+    y=1540
+    while x<y:
+        try:
+            if(direction == 'c'):
+                print('motor running clockwise\n')
+                GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
+                sleep(0.002)
+                GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
+                sleep(0.002)
+                GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
+                sleep(0.002)
+                GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
+                sleep(0.002)
+                x=x+1
+
+            elif(direction == 'a'):
+                print('motor running anti-clockwise\n')
+                GPIO.output(motor_channel, (GPIO.HIGH,GPIO.LOW,GPIO.LOW,GPIO.HIGH))
+                sleep(0.002)
+                GPIO.output(motor_channel, (GPIO.LOW,GPIO.LOW,GPIO.HIGH,GPIO.HIGH))
+                sleep(0.002)
+                GPIO.output(motor_channel, (GPIO.LOW,GPIO.HIGH,GPIO.HIGH,GPIO.LOW))
+                sleep(0.002)
+                GPIO.output(motor_channel, (GPIO.HIGH,GPIO.HIGH,GPIO.LOW,GPIO.LOW))
+                sleep(0.002)
+                x=x+1
+
+                
+        #press ctrl+c for keyboard interrupt
+        except KeyboardInterrupt:
+            #query for setting motor direction or exit
+            motor_direction = input('select motor direction a=anticlockwise, c=clockwise or q=exit: ')
+            #check for exit
+            if(motor_direction == 'q'):
+                print('motor stopped')
+                sys.exit(0)
 try:
     while True:
         blynk.run()
@@ -70,5 +75,3 @@ try:
 except KeyboardInterrupt:
     print("Quit")
 
-# Reset GPIO settings
-GPIO.cleanup()
